dbb1f92b18dbdc3c0795e9e261f9d2c87bd56ce4
isaui <isadestroyed17@gmail.com>
2025-05-29 17:52:20 +0700

    wtf

--- next.config.js
+++
@@ -1,9 +0,0 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {
-  output: 'standalone',
-  // This ensures the Next.js app can be built in environments 
-  // without the specific platform binaries
-  swcMinify: true,
-};
-
-module.exports = nextConfig;
