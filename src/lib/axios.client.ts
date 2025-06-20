744d7b2b7cd703c5103763755f71256041c33435
isaui <isadestroyed17@gmail.com>
2025-06-20 13:27:37 +0700

    fuck fix axiosclient

--- src/lib/axios.client.ts
+++ src/lib/axios.client.ts
@@ -3,7 +3,6 @@
 import axios from "axios";
 
 const axiosClient = axios.create({
-    baseURL: process.env.NEXT_PUBLIC_API_URL || 'http://localhost:8080',
     withCredentials: true,
 })
 export default axiosClient;
